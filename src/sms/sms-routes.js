fe3086a3404148e057755ac12e333b0b0b1eae8e
Subash <Subash@users.noreply.github.com>
2022-06-21 11:21:24 +0545

    Cleanup

--- src/sms/sms-routes.js
+++ src/sms/sms-routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'itty-router';
-import authenticate from '../middlewares/authenticate.js';
+import authenticate from '../auth/authenticate-middleware.js';
 import * as smsHandlers from './sms-handlers.js';
 
 const router = Router({ base: '/api/v1/sms' });
